f26440af1f8f15a5f962d9176faf29947ffce65c
godzz733 <109529335+godzz733@users.noreply.github.com>
2023-03-15 17:14:17 +0900

    [Bronze IV] Title: 기찍 N, Time: 1516 ms, Memory: 32880 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main{
+	public static void main(String[] args) throws IOException{
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		sc.close();
+		for (int i=a; i>0; i--) {
+			System.out.println(i);
+		}
+		
+		
+	}
+}
