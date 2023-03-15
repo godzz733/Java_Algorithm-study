ef97aaeed81dfe56eead6705518ff01eed90ac29
godzz733 <109529335+godzz733@users.noreply.github.com>
2023-03-15 09:36:46 +0900

    [Bronze III] Title: 세 수, Time: 108 ms, Memory: 12896 KB -BaekjoonHub

--- 백준/Bronze/10817. 세 수/세 수.java
+++ 백준/Bronze/10817. 세 수/세 수.java
@@ -1,19 +1,14 @@
-import java.io.BufferedReader;
+
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Comparator;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main{
 	public static void main(String[] args) throws IOException{
 		Scanner sc = new Scanner(System.in);
-		ArrayList arr = new ArrayList();
-		for (int i=0; i<3; i++) {
-			int a = sc.nextInt();
-			arr.add(a);
-		}
-		arr.sort(Comparator.naturalOrder());
-		System.out.println(arr.get(1));
+		int arr[] = {sc.nextInt(), sc.nextInt(), sc.nextInt()};
+		sc.close();
+		Arrays.sort(arr);
+		System.out.println(arr[1]);
 	}
 }
