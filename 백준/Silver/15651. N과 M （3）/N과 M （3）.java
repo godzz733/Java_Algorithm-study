fe53be5a3d3d9589abd12e7ce5fd32768ee5f203
godzz733 <109529335+godzz733@users.noreply.github.com>
2023-03-19 14:38:56 +0900

    [Silver III] Title: N과 M (3), Time: 584 ms, Memory: 270572 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main{
+	
+	static int n,m;
+	static int [] arr;
+	static StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		m = sc.nextInt();
+		arr = new int[m];
+		backtracking(0);
+		System.out.println(sb);
+	}
+	public static void backtracking(int depth) {
+		if (depth==m) {
+			for (int val:arr) {
+				sb.append(val + " ");
+			}
+			sb.append('\n');
+			return;
+		}
+		for (int i = 1; i<n+1; i++) {
+			arr[depth] = i;
+			backtracking(depth+1);
+			}
+		}
+	}
