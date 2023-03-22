652b943efda816f6e847c7626523963284c672d0
godzz733 <109529335+godzz733@users.noreply.github.com>
2023-03-22 22:37:39 +0900

    [Silver IV] Title: 비트가 넘쳐흘러, Time: 80 ms, Memory: 11496 KB -BaekjoonHub

---
+++ 백준/Silver/17419. 비트가 넘쳐흘러/비트가 넘쳐흘러.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main{
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int k = Integer.parseInt(br.readLine());
+		int result = Integer.parseInt(br.readLine(),2);
+		int cnt = 0;
+		while (result>0) {
+			cnt++;
+			result = result-(result&((~result)+1));
+		}
+		System.out.println(cnt);
+	}
+}
