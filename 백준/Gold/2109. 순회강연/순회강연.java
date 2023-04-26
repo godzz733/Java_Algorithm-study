953c423192f4f1fc10a201bb7bec4a908c272189
godzz733 <109529335+godzz733@users.noreply.github.com>
2023-04-26 17:35:24 +0900

    [Gold III] Title: 순회강연, Time: 396 ms, Memory: 24480 KB -BaekjoonHub

---
+++ 백준/Gold/2109. 순회강연/순회강연.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class Main{
+	
+	static class pos{
+		int x,idx;
+		public pos(int x, int idx) {
+			this.x = x;
+			this.idx = idx;
+		}
+	}
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int n = Integer.parseInt(br.readLine());
+		int [] arr = new int[10001];
+		PriorityQueue<pos> q = new PriorityQueue<>((o1,o2) -> Integer.compare(o2.x, o1.x));
+		int result = 0;
+		for (int i=0; i<n; i++) {
+			String [] input = br.readLine().split(" ");
+			int a = Integer.parseInt(input[0]), b = Integer.parseInt(input[1]);
+			q.add(new pos(a,b));
+		}
+		while (!q.isEmpty()) {
+			pos cur = q.poll();
+			int a = cur.x;
+			int b = cur.idx;
+			if (arr[b] == 0) {
+				result += a;
+				arr[b] = 1;
+			} else {
+				while(b>0 && arr[b]!=0) {
+					b--;
+				}
+				if (b>0) {
+					result += a;
+					arr[b] = 1;
+				}
+			}
+		}
+		System.out.println(result);
+	}
+
+}
