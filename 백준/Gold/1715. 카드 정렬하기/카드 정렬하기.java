f04dff16f0ef58d9345545e16c10a9776db5984f
godzz733 <109529335+godzz733@users.noreply.github.com>
2023-05-31 20:58:10 +0900

    [Gold IV] Title: 카드 정렬하기, Time: 372 ms, Memory: 24664 KB -BaekjoonHub

--- 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.java
+++ 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.java
@@ -1,27 +1,29 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
+
 
 public class Main{
+
 	public static void main(String [] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int n = Integer.parseInt(st.nextToken());
-		PriorityQueue<Integer> arr = new PriorityQueue<Integer>();
+		StringTokenizer st;
+		int n = Integer.parseInt(br.readLine());
+		PriorityQueue<Integer> q = new PriorityQueue<>();
 		for (int i=0; i<n; i++) {
-			st = new StringTokenizer(br.readLine());
-			arr.add(Integer.parseInt(st.nextToken()));
+			int tem = Integer.parseInt(br.readLine());
+			q.add(tem);
 		}
-		br.close();
-		long result=0;
-		for (int i=0; i<n-1; i++) {
-			int a = arr.poll();
-			int b = arr.poll();
-			arr.add(a+b);
+		int result = 0;
+		while (q.size() != 1) {
+			int a = q.poll();
+			int b = q.poll();
+			q.add(a+b);
 			result += a+b;
 		}
 		System.out.println(result);
+		
+		
 	}
+
+	
 }
